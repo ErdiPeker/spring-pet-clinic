551170b1532d58d15ebbaba74b85ef043024969e
Erdi Peker <erdipeker@gmail.com>
2023-01-06 17:46:56 +0300

    added bootstrap for data. Closes #14.

--- pet-clinic-web/src/main/java/com/example/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/springpetclinic/controllers/OwnerController.java
@@ -2,8 +2,8 @@ package com.example.springpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-@RequestMapping("/owners")
-@Controller
+ @Controller
+ @RequestMapping("/owners")
 public class OwnerController {
     @RequestMapping({"","/","/index","/index.html"})
     public String listOwners(){
