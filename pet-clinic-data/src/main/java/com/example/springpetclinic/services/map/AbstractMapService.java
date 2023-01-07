36f87e42db4e17b9aa3c494d90b988991b23fb8d
Erdi Peker <erdipeker@gmail.com>
2023-01-07 14:34:54 +0300

    refactored Map Impl to abstract ıd generation. Closes #23.

--- pet-clinic-data/src/main/java/com/example/springpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/services/map/AbstractMapService.java
@@ -1,30 +1,48 @@
 package com.example.springpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.example.springpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T,ID> {
-    protected Map<ID,T> map = new HashMap<>();
+import java.util.*;
 
-    Set<T> findAll(){
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
+
+    Set<T> findAll() {
         return new HashSet<>(map.values());
     }
-    T findById(ID id){
+
+    T findById(ID id) {
         return map.get(id);
     }
 
-    T save(ID id,T obj){
-        map.put(id,obj);
+    T save(T obj) {
+        if (obj != null) {
+            if (obj.getId() == null) {
+                obj.setId(getNextId());
+            }
+            map.put(obj.getId(), obj);
+
+        } else {
+            throw new RuntimeException("Object can not be null");
+        }
         return obj;
     }
 
-    void deleteById(ID id){
+    void deleteById(ID id) {
         map.remove(id);
     }
 
-    void delete(T obj){
+    void delete(T obj) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(obj));
+    }
+
+    private Long getNextId() {
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        }catch (NoSuchElementException e){
+            nextId=1L;
+        }
+        return nextId;
     }
 }
